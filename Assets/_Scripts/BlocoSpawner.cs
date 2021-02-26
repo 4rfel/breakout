94c08c403630af80d1ce57bcdee214c2d1a3c84c
Arfel <rafael_1999_@hotmail.com.br>
2021-02-26 18:12:31 -0300

    a

--- Assets/_Scripts/BlocoSpawner.cs
+++ Assets/_Scripts/BlocoSpawner.cs
@@ -6,16 +6,20 @@ public class BlocoSpawner : MonoBehaviour
 {
     [SerializeField] private UI_pontos ui_points;
     [SerializeField] private GameObject Bloco;
+
+
     [SerializeField] private AudioSource audioData;
     [SerializeField] private ParticleSystem exp;
 
     void Start()
     {
-        for(int i = 0; i < 11; i++)
+        int n = 10;
+        Debug.Log(n + " " + Bloco.GetComponent<SpriteRenderer>().sprite.rect.width);
+        for (int i = 0; i < n; i++)
         {
             for(int j = 0; j < 4; j++)
             {
-                Vector3 posicao = new Vector3(-8 + 1.55f * i, 4 - 0.55f * j);
+                Vector3 posicao = new Vector3(-7 + 1.55f * i, 4 - 0.55f * j);
 
                 GameObject Brick = Instantiate(Bloco, posicao, Quaternion.identity, transform);
                 Brick.GetComponent<Bloco>().setDurability(4 - j);
